deaeba9eef442c84500157a8da47343362d522ae
Trieu Bui <vantrieu.ou@gmail.com>
2019-12-16 11:47:21 +0700

    add editor config for vim

---
+++ setup_environment/editor_config/npm_eslint_prettier_setup.sh
@@ -0,0 +1,3 @@
+npm i -D eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
+npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+npm i -D babel-eslint
