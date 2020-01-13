30dc0c05402b04e240bac99cf0ca41b84ed389f1
Trieu Bui <vantrieu.ou@gmail.com>
2020-01-13 14:27:04 +0700

    adjust formatter + linter

--- setup_environment/editor_config/npm_eslint_prettier_setup.sh
+++ setup_environment/editor_config/npm_eslint_prettier_setup.sh
@@ -1,3 +1,4 @@
-npm i -D eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
-npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+# Repect to https://www.rockyourcode.com/es-lint-and-prettier-for-java-script-react-in-vim/
+# npm i -D eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react
+npm i -D eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-react
 npm i -D babel-eslint
